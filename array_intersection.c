328369e4ce199ed8a9af52ba125e2d661c52d2aa
HassaneSass07 <sasshassane02@gmail.com>
2025-05-30 06:24:48 +0100

    barka

--- array_intersection.c
+++ array_intersection.c
@@ -1,54 +1,62 @@
+// array_intersection.c
+// Programme qui affiche l'intersection de deux tableaux (éléments communs sans doublons)
+
 #include <stdio.h>
 
 int main() {
     int n1, n2;
 
-    // Lecture du premier tableau
+    // Lecture de la taille du premier tableau
     scanf("%d", &n1);
     int a[n1];
-    for (int i = 0; i < n1; i++)
+    for (int i = 0; i < n1; i++) {
         scanf("%d", &a[i]);
+    }
 
-    // Lecture du deuxième tableau
+    // Lecture de la taille du deuxième tableau
     scanf("%d", &n2);
     int b[n2];
-    for (int i = 0; i < n2; i++)
+    for (int i = 0; i < n2; i++) {
         scanf("%d", &b[i]);
+    }
 
-    int intersection[n1 < n2 ? n1 : n2]; // Taille max = min(n1,n2)
+    // Tableau temporaire pour stocker les éléments communs sans doublons
+    int intersection[n1 < n2 ? n1 : n2];
     int taille_intersection = 0;
 
-    // Chercher les éléments communs
+    // Parcours de tous les éléments du premier tableau
     for (int i = 0; i < n1; i++) {
-        // Vérifier si a[i] est dans b
-        int dans_b = 0;
-        for (int j = 0; j < n2; j++) {
-            if (a[i] == b[j]) {
-                dans_b = 1;
-                break;
-            }
-        }
-        if (!dans_b)
-            continue;
+        int deja_present = 0;  // Flag pour éviter les doublons dans l'intersection
 
-        // Vérifier que a[i] n'est pas déjà dans intersection
-        int deja_present = 0;
+        // Vérifier si a[i] est déjà dans intersection
         for (int k = 0; k < taille_intersection; k++) {
             if (intersection[k] == a[i]) {
                 deja_present = 1;
                 break;
             }
         }
-        if (!deja_present) {
-            intersection[taille_intersection++] = a[i];
+        if (deja_present) continue;  // Ignorer cet élément s'il est déjà enregistré
+
+        // Vérifier si a[i] est présent dans le deuxième tableau b
+        int present_dans_b = 0;
+        for (int j = 0; j < n2; j++) {
+            if (a[i] == b[j]) {
+                present_dans_b = 1;
+                break;
+            }
+        }
+
+        // Ajouter à l'intersection si trouvé dans b
+        if (present_dans_b) {
+            intersection[taille_intersection] = a[i];
+            taille_intersection++;
         }
     }
 
-    // Affichage de l'intersection
+    // Affichage du résultat
+    printf("Intersection :");
     for (int i = 0; i < taille_intersection; i++) {
-        printf("%d", intersection[i]);
-        if (i < taille_intersection - 1)
-            printf(" ");
+        printf(" %d", intersection[i]);
     }
     printf("\n");
 
