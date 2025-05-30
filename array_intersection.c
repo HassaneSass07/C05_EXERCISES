30d655683994d0c974b5aa315b5800b649572f20
HassaneSass07 <sasshassane02@gmail.com>
2025-05-30 03:50:00 +0100

    rivals

--- array_intersection.c
+++ array_intersection.c
@@ -3,13 +3,13 @@
 int main() {
     int n1, n2;
 
-    // Lire la taille du premier tableau et ses éléments
+    // Lire la taille et les éléments du premier tableau
     scanf("%d", &n1);
     int a[n1];
     for (int i = 0; i < n1; i++)
         scanf("%d", &a[i]);
 
-    // Lire la taille du deuxième tableau et ses éléments
+    // Lire la taille et les éléments du second tableau
     scanf("%d", &n2);
     int b[n2];
     for (int i = 0; i < n2; i++)
@@ -17,19 +17,18 @@ int main() {
 
     // Parcourir le premier tableau
     for (int i = 0; i < n1; i++) {
-        int est_deja_affiche = 0;
+        int deja_affiche = 0;
 
-        // Vérifier si a[i] a déjà été affiché
-        for (int k = 0; k < i; k++) {
-            if (a[i] == a[k]) {
-                est_deja_affiche = 1;
+        // Éviter les doublons : vérifier si a[i] a déjà été affiché
+        for (int j = 0; j < i; j++) {
+            if (a[i] == a[j]) {
+                deja_affiche = 1;
                 break;
             }
         }
-        if (est_deja_affiche)
-            continue;
+        if (deja_affiche) continue;
 
-        // Vérifier si a[i] existe dans le tableau b[]
+        // Vérifier si a[i] existe dans le second tableau
         for (int j = 0; j < n2; j++) {
             if (a[i] == b[j]) {
                 printf("%d ", a[i]);
