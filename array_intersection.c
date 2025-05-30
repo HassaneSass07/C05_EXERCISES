38a66dabe29b64293903565021e5c0b1628a5ded
HassaneSass07 <sasshassane02@gmail.com>
2025-05-30 04:39:46 +0100

    bulls

--- array_intersection.c
+++ array_intersection.c
@@ -1,42 +1,78 @@
 #include <stdio.h>
+#include <stdbool.h> // For boolean type
+#include <stdlib.h>  // For malloc and free
+
+// Function to check if an element is already in an array (for uniqueness)
+bool isElementInArray(int *arr, int size, int element) {
+    for (int i = 0; i < size; i++) {
+        if (arr[i] == element) {
+            return true;
+        }
+    }
+    return false;
+}
 
 int main() {
     int n1, n2;
 
-    // Lecture du premier tableau
+    // Get size of the first array
+    printf("Enter the size of the first array: ");
     scanf("%d", &n1);
-    int a[n1];
-    for (int i = 0; i < n1; i++)
-        scanf("%d", &a[i]);
 
-    // Lecture du deuxième tableau
-    scanf("%d", &n2);
-    int b[n2];
-    for (int i = 0; i < n2; i++)
-        scanf("%d", &b[i]);
-
-    // Affichage des éléments communs sans doublons
+    int arr1[n1];
+    printf("Enter %d elements for the first array:\n", n1);
     for (int i = 0; i < n1; i++) {
-        int deja_vu = 0;
+        scanf("%d", &arr1[i]);
+    }
 
-        // Vérifie si a[i] a déjà été traité
-        for (int k = 0; k < i; k++) {
-            if (a[i] == a[k]) {
-                deja_vu = 1;
-                break;
-            }
-        }
-        if (deja_vu) continue;
+    // Get size of the second array
+    printf("Enter the size of the second array: ");
+    scanf("%d", &n2);
 
-        // Vérifie si a[i] est présent dans b[]
+    int arr2[n2];
+    printf("Enter %d elements for the second array:\n", n2);
+    for (int i = 0; i < n2; i++) {
+        scanf("%d", &arr2[i]);
+    }
+
+    // Create a temporary array to store intersection elements
+    // The maximum possible intersection size is the minimum of n1 and n2
+    int maxIntersectionSize = (n1 < n2) ? n1 : n2;
+    int *intersectionArray = (int *)malloc(maxIntersectionSize * sizeof(int));
+    if (intersectionArray == NULL) {
+        printf("Memory allocation failed!\n");
+        return 1;
+    }
+
+    int intersectionCount = 0;
+
+    // Find common elements
+    for (int i = 0; i < n1; i++) {
         for (int j = 0; j < n2; j++) {
-            if (a[i] == b[j]) {
-                printf("%d ", a[i]);
-                break;
+            if (arr1[i] == arr2[j]) {
+                // Check if the element is already added to avoid duplicates in intersection
+                if (!isElementInArray(intersectionArray, intersectionCount, arr1[i])) {
+                    intersectionArray[intersectionCount] = arr1[i];
+                    intersectionCount++;
+                }
+                break; // Move to the next element in arr1 once a match is found
             }
         }
     }
 
-    printf("\n");
+    // Print the intersection
+    printf("Intersection : ");
+    if (intersectionCount == 0) {
+        printf("No common elements.\n");
+    } else {
+        for (int i = 0; i < intersectionCount; i++) {
+            printf("%d ", intersectionArray[i]);
+        }
+        printf("\n");
+    }
+
+    // Free dynamically allocated memory
+    free(intersectionArray);
+
     return 0;
 }
