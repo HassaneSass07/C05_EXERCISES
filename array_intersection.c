d28b4386e71923816d2de43fb4e99ad6bc8c4e91
HassaneSass07 <sasshassane02@gmail.com>
2025-05-30 18:34:32 +0100

    maitres

--- array_intersection.c
+++ array_intersection.c
@@ -1,13 +1,12 @@
 #include <stdio.h>
 
-// Fonction pour vérifier si un élément est déjà présent dans un tableau
-int est_deja_present(int tab[], int taille, int valeur) {
-    for (int i = 0; i < taille; i++) {
-        if (tab[i] == valeur) {
-            return 1; // trouvé
-        }
+// Fonction qui vérifie si un élément est déjà apparu dans un tableau jusqu'à l'index donné
+int deja_present(int tab[], int taille, int valeur, int limite) {
+    for (int i = 0; i < limite; i++) {
+        if (tab[i] == valeur)
+            return 1; // Oui, déjà présent
     }
-    return 0; // non trouvé
+    return 0; // Non, pas encore rencontré
 }
 
 int main() {
@@ -25,30 +24,20 @@ int main() {
     for (int i = 0; i < n2; i++)
         scanf("%d", &b[i]);
 
-    int intersection[n1 + n2]; // Tableau pour stocker les éléments communs
-    int taille_intersection = 0;
-
-    // Pour chaque élément de a[], vérifier s'il est dans b[] et pas déjà ajouté
+    // Affichage de l'intersection sans doublons
+    printf("Intersection :");
     for (int i = 0; i < n1; i++) {
-        // Vérifie s'il existe dans b[]
+        // Vérifie si a[i] existe dans b[]
         for (int j = 0; j < n2; j++) {
             if (a[i] == b[j]) {
-                // Vérifie qu'il n'est pas déjà dans l'intersection
-                if (!est_deja_present(intersection, taille_intersection, a[i])) {
-                    intersection[taille_intersection] = a[i];
-                    taille_intersection++;
-                }
-                break; // inutile de continuer à chercher dans b[]
+                // Vérifie si ce n'est pas déjà affiché (doublon dans a[])
+                if (!deja_present(a, n1, a[i], i))
+                    printf(" %d", a[i]);
+                break;
             }
         }
     }
 
-    // Affichage du résultat
-    printf("Intersection :");
-    for (int i = 0; i < taille_intersection; i++) {
-        printf(" %d", intersection[i]);
-    }
     printf("\n");
-
     return 0;
 }
