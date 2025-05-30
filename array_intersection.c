079290e5dd0f82beb037182343738dd2d02863a2
HassaneSass07 <sasshassane02@gmail.com>
2025-05-30 06:53:09 +0100

    virgule

--- array_intersection.c
+++ array_intersection.c
@@ -3,24 +3,23 @@
 int main() {
     int n1, n2;
 
-    // Lire la taille du premier tableau
     scanf("%d", &n1);
     int a[n1];
-    for (int i = 0; i < n1; i++)
+    for (int i = 0; i < n1; i++) {
         scanf("%d", &a[i]);
+    }
 
-    // Lire la taille du deuxième tableau
     scanf("%d", &n2);
     int b[n2];
-    for (int i = 0; i < n2; i++)
+    for (int i = 0; i < n2; i++) {
         scanf("%d", &b[i]);
+    }
 
-    int intersection[n1 < n2 ? n1 : n2]; // Tableau pour stocker les éléments communs
-    int count = 0; // Nombre d'éléments communs uniques trouvés
+    int max_size = (n1 < n2) ? n1 : n2;
+    int intersection[max_size];
+    int count = 0;
 
-    // Parcourir chaque élément du tableau a
     for (int i = 0; i < n1; i++) {
-        // Vérifier si a[i] est dans b
         int found_in_b = 0;
         for (int j = 0; j < n2; j++) {
             if (a[i] == b[j]) {
@@ -30,7 +29,6 @@ int main() {
         }
 
         if (found_in_b) {
-            // Vérifier si a[i] n'est pas déjà dans intersection
             int already_added = 0;
             for (int k = 0; k < count; k++) {
                 if (intersection[k] == a[i]) {
@@ -39,7 +37,6 @@ int main() {
                 }
             }
 
-            // Ajouter a[i] s'il n'est pas encore présent
             if (!already_added) {
                 intersection[count] = a[i];
                 count++;
@@ -47,10 +44,12 @@ int main() {
         }
     }
 
-    // Affichage final
-    printf("Intersection :");
-    for (int i = 0; i < count; i++)
-        printf(" %d", intersection[i]);
+    if (count > 0) {
+        printf("%d", intersection[0]);
+        for (int i = 1; i < count; i++) {
+            printf(" %d", intersection[i]);
+        }
+    }
     printf("\n");
 
     return 0;
