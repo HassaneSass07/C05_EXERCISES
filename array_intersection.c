2b290fdc27246ae91fc67fd624f2f0344d246662
HassaneSass07 <sasshassane02@gmail.com>
2025-05-30 03:24:25 +0100

    aes

--- array_intersection.c
+++ array_intersection.c
@@ -2,36 +2,33 @@
 
 int main() {
     int n1, n2;
-    
-    // Lire la taille du premier tableau
     scanf("%d", &n1);
     int a[n1];
     for (int i = 0; i < n1; i++)
-        scanf("%d", &a[i]);  // Remplir le tableau a[]
+        scanf("%d", &a[i]);
 
-    // Lire la taille du deuxième tableau
     scanf("%d", &n2);
     int b[n2];
     for (int i = 0; i < n2; i++)
-        scanf("%d", &b[i]);  // Remplir le tableau b[]
+        scanf("%d", &b[i]);
 
-    // Parcourir chaque élément du tableau a[]
-    for (int i = 0; i < n1; i++) {
-        int deja_vu = 0;
+    // Parcours du 2e tableau (ordre d’affichage basé sur b[])
+    for (int i = 0; i < n2; i++) {
+        int deja_affiche = 0;
 
-        // Vérifie si l'élément a[i] a déjà été traité
-        for (int j = 0; j < i; j++) {
-            if (a[i] == a[j]) {
-                deja_vu = 1;
+        // Éviter les doublons dans b[]
+        for (int k = 0; k < i; k++) {
+            if (b[i] == b[k]) {
+                deja_affiche = 1;
                 break;
             }
         }
-        if (deja_vu) continue;
+        if (deja_affiche) continue;
 
-        // Vérifie si a[i] est présent dans le tableau b[]
-        for (int j = 0; j < n2; j++) {
-            if (a[i] == b[j]) {
-                printf("%d ", a[i]); // Affiche l'élément commun une seule fois
+        // Vérifie si b[i] est présent dans a[]
+        for (int j = 0; j < n1; j++) {
+            if (b[i] == a[j]) {
+                printf("%d ", b[i]);
                 break;
             }
         }
