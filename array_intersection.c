d8261a916cc4e03144aac3dd0d2f0f59f58ed99d
HassaneSass07 <sasshassane02@gmail.com>
2025-05-30 04:27:42 +0100

    jamais

--- array_intersection.c
+++ array_intersection.c
@@ -3,58 +3,40 @@
 int main() {
     int n1, n2;
 
-    // Lire les tailles et les tableaux
+    // Lecture du premier tableau
     scanf("%d", &n1);
     int a[n1];
     for (int i = 0; i < n1; i++)
         scanf("%d", &a[i]);
 
+    // Lecture du deuxième tableau
     scanf("%d", &n2);
     int b[n2];
     for (int i = 0; i < n2; i++)
         scanf("%d", &b[i]);
 
-    // Préparer l'espace pour l'intersection (taille minimale)
-    int max_size;
-    if (n1 < n2)
-        max_size = n1;
-    else
-        max_size = n2;
-    int inter[max_size];
-    int sz = 0;
-
-    // Remplir inter[] avec les éléments communs (sans doublons)
+    // Affichage des éléments communs sans doublons
     for (int i = 0; i < n1; i++) {
-        // Vérifier présence dans b[]
-        int in_b = 0;
+        int deja_affiche = 0;
+
+        // Vérifier si l'élément a[i] a déjà été traité
+        for (int k = 0; k < i; k++) {
+            if (a[i] == a[k]) {
+                deja_affiche = 1;
+                break;
+            }
+        }
+        if (deja_affiche) continue;
+
+        // Vérifier si a[i] existe dans b[]
         for (int j = 0; j < n2; j++) {
             if (a[i] == b[j]) {
-                in_b = 1;
+                printf("%d ", a[i]);
                 break;
             }
         }
-        if (!in_b)
-            continue;
-
-        // Vérifier doublon dans inter[]
-        int in_inter = 0;
-        for (int k = 0; k < sz; k++) {
-            if (a[i] == inter[k]) {
-                in_inter = 1;
-                break;
-            }
-        }
-        if (!in_inter)
-            inter[sz++] = a[i];
     }
 
-    // Afficher l'intersection
-    for (int i = 0; i < sz; i++) {
-        printf("%d", inter[i]);
-        if (i + 1 < sz)
-            printf(" ");
-    }
     printf("\n");
-
     return 0;
 }
