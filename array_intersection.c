aa2e43ae03edf3778322ad7bdfac6bb0acb4ef1f
HassaneSass07 <sasshassane02@gmail.com>
2025-05-30 03:38:10 +0100

    elektrik

--- array_intersection.c
+++ array_intersection.c
@@ -2,41 +2,40 @@
 
 int main() {
     int n1, n2;
-
-    // Lire la taille du premier tableau
+    
+    // Lire les tailles
     scanf("%d", &n1);
     int a[n1];
     for (int i = 0; i < n1; i++)
         scanf("%d", &a[i]);
 
-    // Lire la taille du deuxième tableau
     scanf("%d", &n2);
     int b[n2];
     for (int i = 0; i < n2; i++)
         scanf("%d", &b[i]);
 
-    // Parcourir le premier tableau
+    // Parcourir le tableau a[]
     for (int i = 0; i < n1; i++) {
-        int deja_imprime = 0;
+        int deja_vu = 0;
 
-        // Vérifie si l'élément a[i] a déjà été traité dans a[]
+        // Vérifie si a[i] est déjà apparu avant dans a[]
         for (int j = 0; j < i; j++) {
             if (a[i] == a[j]) {
-                deja_imprime = 1;
+                deja_vu = 1;
                 break;
             }
         }
-      
+        if (deja_vu) continue;
 
-        // Cherche a[i] dans b[]
+        // Vérifie si a[i] existe dans b[]
         for (int j = 0; j < n2; j++) {
             if (a[i] == b[j]) {
-                printf("%d ", a[i]);  // Affiche l’élément commun
+                printf("%d ", a[i]);
                 break;
             }
         }
     }
 
-    printf("\n");  // Retour à la ligne final
+    printf("\n");
     return 0;
 }
